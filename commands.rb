6a5d689ef35287fe29a8b3a28008f251f4e109b4
Pierre THierry <pierre@nothos.net>
2012-11-03 12:42:09 +0100

    Add command classes

---
+++ commands.rb
@@ -0,0 +1,31 @@
+
+class Show
+  attr_accessor :bits
+  def initialize(*bits)
+    @bits = bits
+  end
+end
+
+class Shift
+  attr_accessor :direction # True is left
+  attr_accessor :width
+  def initialize(dir = True, width = 1)
+    @direction = dir
+    @width = width
+  end
+end
+
+class Located_command
+  attr_accessor :start
+  attr_accessor :end
+  def initialize(start = 0, end = -1)
+    @start = start
+    @end = end
+  end
+end
+
+class Emphasis < Located_command
+end
+
+class Erase < Located_command
+end
