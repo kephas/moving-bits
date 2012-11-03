eb79e9c3c09b2bd4f786c1ac832c9d15908e1118
HarnoRanaivo <kshegzyaj@gmail.com>
2012-11-03 14:00:02 +0100

    first commit to masks

--- masks.rb
+++ masks.rb
@@ -4,19 +4,27 @@ class and
         @word1 = word1
         @word2 = word2
         @result = self.calcresult
-        @steps = self.calcsteps
+        @animate = self.calcanimate
     end
 
     def calcresult
         tmptab = []
+        i = 0
 
-        unless word[i] == nil done
-        tmptab.push
+        unless (word1[i] == nil) || (word2[i] == nil) do
+            if word1[i] == word2[i]
+                if word[i] == 1
+                    tmptab.puts(1)
+                end
+            else
+                tmptab.puts(0)
+            end
         end
-        return 
+
+        return tmptab
     end
 
-    def calcsteps
-        return 
+    def calcanimate
+        return [Show.new(word1), Show.new(word2), Show.new(@result)]
     end
 end
