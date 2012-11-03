7c06a40b7fd9a203886f3c7a033fa0e86ad06219
Pierre Thiery <pierre@nothos.net>
2012-11-03 17:07:22 +0100

    Implement show command

--- terminal.rb
+++ terminal.rb
@@ -15,4 +15,27 @@ class Text_UI
       end
     end
   end
+
+  def show(command)
+    word = command.word.bits.join('')
+    operator = if command.operator then command.operator else ''end
+    operand = if command.operand then
+                case command.operand
+                when Integer then command.operand.to_s
+                when Word then command.operand.bits.join('')
+                when String then command.operand
+                end
+              else ''
+              end
+
+    length1 = word.length
+    length2 = operator.length + 2 + operand.length
+    padding1 = if length2 > length1 then
+                 Array.new(length2 - length1, ' ').join('')
+                 else ''
+                 end
+
+    print padding1, word, "\n", operator, '  ', operand, "\n"
+    @word = command.word
+  end
 end
