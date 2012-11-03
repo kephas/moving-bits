036a8e77f6db179070e0feebc37863791d9fd9e0
HarnoRanaivo <kshegzyaj@gmail.com>
2012-11-03 13:49:33 +0100

    first commit for masks

---
+++ masks.rb
@@ -0,0 +1,22 @@
+class and
+    attr_accessor :word1, :word2, :result, :steps
+    def initialize(word1, word2)
+        @word1 = word1
+        @word2 = word2
+        @result = self.calcresult
+        @steps = self.calcsteps
+    end
+
+    def calcresult
+        tmptab = []
+
+        unless word[i] == nil done
+        tmptab.push
+        end
+        return 
+    end
+
+    def calcsteps
+        return 
+    end
+end
