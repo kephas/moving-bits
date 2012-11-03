bd65aadb0a3bdcb58a5e66dcd0dd5c8dfafd22fe
HarnoRanaivo <kshegzyaj@gmail.com>
2012-11-03 14:38:40 +0100

    first go at and and or

--- masks.rb
+++ masks.rb
@@ -1,4 +1,4 @@
-class and
+class wand
     attr_accessor :word1, :word2, :result, :steps
     def initialize(word1, word2)
         @word1 = word1
@@ -11,7 +11,7 @@ class and
         tmptab = []
         i = 0
 
-        unless (word1[i] == nil) || (word2[i] == nil) do
+        unless (word1[i] == nil) || (word2[i] == nil)
             if word1[i] == word2[i]
                 if word[i] == 1
                     tmptab.puts(1)
@@ -19,9 +19,40 @@ class and
             else
                 tmptab.puts(0)
             end
+            i += 1
         end
 
-        return tmptab
+        return @tmptab
+    end
+
+    def calcanimate
+        return [Show.new(word1), Show.new(word2), Show.new(@result)]
+    end
+end
+
+class wor
+    attr_accessor :word1, :word2, :result, :steps
+    def initialize(word1, word2)
+        @word1 = word1
+        @word2 = word2
+        @result = self.calcresult
+        @animate = self.calcanimate
+    end
+
+    def calcresult
+        tmptab = []
+        i = 0
+
+        unless (word1[i] == nil) || (word2[i] == nil)
+            if (word1[i] == 1) || (word2[i] == 1)
+                tmptab.puts(1)
+            else
+                tmptab.puts(0)
+            end
+            i += 1
+        end
+    
+        return @tmptab
     end
 
     def calcanimate
