a496f0a761fd45fdab5b7e9290984da3686ecc10
Xavier <xavier.nunn@larez.fr>
2012-11-24 16:07:57 +0100

    Début d'une tentative de connexion avec facebook. Attention ! J'ai modifié l'action base#index, ce qui fait que la page d'acceuil va rediriger vers FB. C'est temporaire évidemment. De plus, ajout de la gemme fb_graph.

--- rails/moving-bits/config/routes.rb
+++ rails/moving-bits/config/routes.rb
@@ -1,4 +1,8 @@
 MovingBits::Application.routes.draw do
+  resources :users
+
+  match "facebook/callback" => "base#fb_callback"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ MovingBits::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'index#base'
+  root :to => 'base#index'
 
   # See how all your routes lay out with "rake routes"
 
